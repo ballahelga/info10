e0fc89ec85af2125753052090520968be1c3c972
ballahelga <47588042+ballahelga@users.noreply.github.com>
2019-10-09 10:21:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,20 +3,41 @@
 using namespace std;
 
 int main()
-
 {
-     int i=0,a=1;
-     double s=0;
 
-     while(a>=0) {
-     cout<<"a=";
-      cin>>a;
-       if(a>0 && a%2==0){
-        s=s+a;
+     int a,max=-INT_MAX,max2=-INT_MAX+11,max2=-INT_MAX+2, min=INT_MAX, min2=INT_MAX+1, min3=INT_MAX+2,i;
+     double s=1,n=0;
+
+   for(i=0;i<7;i++){
+    cout<<"a="<< i <<". szam:";
+        cin>>a;
+      if(a>max) {
+           max3=max2;
+           max2=max;
+           max=a;
+        }else if()
+        if (a<min){
+          min2=min
+          min=a;
+        }
+
+   }
+    a= max;
+   int b= min;
+
+   while(a!=b) {
+        if(a>b) {
+           a=a-b;
+        }
+        else {
+           b=b-a ;
+        }
        }
-    }
 
-  cout <<"a parosak osszege=" << s << endl;
+
+cout <<"lnko" << a << endl;
+cout <<"lkkt" << max*min/a << endl;
     return 0;
 
+    return 0;
 }
