da0b96010d782c3cc4f8c01658a047f0ffc5cb02
ballahelga <47588042+ballahelga@users.noreply.github.com>
2019-10-16 09:54:46 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,40 +4,25 @@ using namespace std;
 
 int main()
 {
-
-     int a,max=-INT_MAX,max2=-INT_MAX+11,max2=-INT_MAX+2, min=INT_MAX, min2=INT_MAX+1, min3=INT_MAX+2,i;
-     double s=1,n=0;
-
-   for(i=0;i<7;i++){
-    cout<<"a="<< i <<". szam:";
-        cin>>a;
-      if(a>max) {
-           max3=max2;
-           max2=max;
-           max=a;
-        }else if()
-        if (a<min){
-          min2=min
-          min=a;
+    int oszto=2,a;
+    bool prim=true;
+    cout<<"a=";
+    cin>>a;
+    for(oszto=2; oszto<a/2; oszto++)
+    {
+        if(a%oszto==0)
+        {
+            prim=false;
         }
 
-   }
-    a= max;
-   int b= min;
-
-   while(a!=b) {
-        if(a>b) {
-           a=a-b;
-        }
-        else {
-           b=b-a ;
-        }
-       }
-
-
-cout <<"lnko" << a << endl;
-cout <<"lkkt" << max*min/a << endl;
-    return 0;
-
+    }
+    if(prim)
+    {
+        cout <<"a szam prim";
+    }
+    else
+    {
+        cout <<"a szam nem prim";
+    }
     return 0;
 }
