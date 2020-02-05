5572845e80151ac751b16a6d448506aed197c7e1
ballahelga <47588042+ballahelga@users.noreply.github.com>
2020-02-05 10:10:34 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,25 +4,28 @@ using namespace std;
 
 int main()
 {
-    int oszto=2,a;
-    bool prim=true;
-    cout<<"a=";
-    cin>>a;
-    for(oszto=2; oszto<a/2; oszto++)
-    {
-        if(a%oszto==0)
-        {
-            prim=false;
-        }
+   int tomb[1000];
+   int n,i,j,aux=0;
+   cout<<"n=";
+   cin>>n;
+   for(i=0; i<n;i++){
+    cout<<"tomb["<<i<<"]=";
+    cin>>tomb[i];}
+   for(i=0; i<n;i++){
+   cout<< tomb[i] <<" ";
+   }
+   cout<<endl;
+    for(i=0;i<n-1;i++)
+    for(j=i+1;j<n;j++){
+         if(tomb[i]>tomb[j]){
+            aux=tomb[j];
+            tomb[j]=tomb[i];
+            tomb[i]=aux;
+    }
+   }
+   for(i=0; i<n;i++){
+   cout<< tomb[i] <<" ";
+   }
 
-    }
-    if(prim)
-    {
-        cout <<"a szam prim";
-    }
-    else
-    {
-        cout <<"a szam nem prim";
-    }
     return 0;
 }
